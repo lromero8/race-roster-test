9af1a102a81970304dab51c9886944037b695d1e
LROMERO8 <lromero_rsx@hotmail.com>
2021-04-30 23:59:59 -0400

    navbar and home components created

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Generic logo</h1>
+            <div className="links">
+                <a href="/">Login</a>
+                <a href="/">Signup</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
