a85de7f031986867619743cfb7363be77b50c646
LROMERO8 <lromero_rsx@hotmail.com>
2021-05-02 19:32:38 -0400

    header done

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white">
+            
+                <h1 className="mt-5 mb-3">Make event management a breeze</h1>
+
+                <h3 className="mb-4">ACME Events Inc. makes making beautiful event landing pages a DIY adventure.</h3>
+
+                <input className="demoButton" type="button" value="BOOK A DEMO"/>
+
+            </div>
+
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,16 @@
+import logo from './Assets/Logo.png'
+
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Generic logo</h1>
-            <div className="links">
-                <a href="/">Login</a>
-                <a href="/">Signup</a>
+            <div className="container-fluid">
+                
+                <a className="navbar-brand" href="/#"><img src={logo} alt="logo"/></a>
+
+                <div className="d-flex">
+                    <a className="loginButton nav-link" href="/#">LOG-IN</a>
+                    <input className="signupButton" type="button" value="SIGN UP"/>
+                </div>
             </div>
         </nav>
     );
