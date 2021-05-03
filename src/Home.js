a27c4a467f2cbb5ef86211f7c9654a05b062cf91
LROMERO8 <lromero_rsx@hotmail.com>
2021-05-02 22:43:37 -0400

    section finished

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
+import heartLamps from './Assets/heart-lamps.png'
+
 const Home = () => {
     return (
         <div className="home">
-            <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white">
+
+            <div className="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white">
             
                 <h1 className="mt-5 mb-3">Make event management a breeze</h1>
 
@@ -11,10 +14,35 @@ const Home = () => {
 
             </div>
 
+            {/* <section> */}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-xl-4">
+                            <img className="lampImg" src={heartLamps} alt="heart lamp"/>
+                        </div>
+                        <div className="col-lg-6 col-xl-8 text-left pl-xl-5 mt-2">
+                            <h2 className="mb-4">Make your ticket holders happy</h2>
+                            <p>
+                                A beautiful little sunset. Talent is a pursued interest. That is to say, anything you practice you can do. This
+                                is probably the greatest thing that's ever happened in my life. These things happen automatically. All you
+                                have to do is just let them happen.
+                            </p>
+                            <p>
+                                Just let go - and fall like a little waterfall. Nothing's gonna make your husband or wife madder than coming
+                                home and having a snow-covered dinner. Just pretend you are a whisper floating across a mountain. Zip.
+                                That easy.
+                            </p>
+                            <a className="featureLink" href="/#">View full feature list</a>
+                        </div>
+                    </div>
+                </div>
+            {/* </section> */}
+
+
+            {/* <br/><br/><br/><br/><br/><br/><br/><br/><br/>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/> */}
         </div>
     );
 }
