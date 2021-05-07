85436a5eac5e948d2f54db02aec263f65c0a4ae1
LROMERO8 <lromero_rsx@hotmail.com>
2021-05-06 20:44:00 -0400

    navbar optimized for mobile

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,12 @@ import logo from './Assets/Logo.png'
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="container-fluid">
-                
-                <a className="navbar-brand" href="/#"><img src={logo} alt="logo"/></a>
+        <nav className="navbar">                
+            <a className="navbar-brand" href="/#"><img src={logo} alt="logo"/></a>
 
-                <div className="d-flex">
-                    <a className="loginButton nav-link" href="/#">LOG-IN</a>
-                    <input className="signupButton" type="button" value="SIGN UP"/>
-                </div>
+            <div className="d-flex">
+                <a className="loginButton nav-link" href="/#">LOG-IN</a>
+                <input className="signupButton" type="button" value="SIGN UP"/>
             </div>
         </nav>
     );
